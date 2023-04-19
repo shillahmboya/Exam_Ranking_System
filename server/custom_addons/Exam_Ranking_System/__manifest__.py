4fe2ab685bf6fa52db293c9f74cae78befc702fb
Martin Maina <mainamartin023@gmail.com>
2023-04-19 15:17:23 +0300

    update manifest

--- server/custom_addons/Exam_Ranking_System/__manifest__.py
+++ server/custom_addons/Exam_Ranking_System/__manifest__.py
@@ -19,24 +19,4 @@
     'auto_install': False,
     'application': True,
 }
-{
-    'name': 'Exams Ranking System',
-    'version': '1.0',
-    'author': 'Shillah',
-    'website': 'https://www.ex.com',
-    'summary': 'A module to manage exam results and rankings',
-    'description': 'This module allows users to create exams, add students, enter scores, and rank students based on their exam results.',
-    'category': 'Education',
-    'depends': ['base'],
-    'data': [
-        'views/exam_views.xml',
-        'views/student_views.xml',
-        'views/exam_result_views.xml',
-        'security/exam_security.xml',
-        'security/ir.model.access.csv',
-    ],
-    'demo': [],
-    'installable': True,
-    'auto_install': False,
-    'application': True,
-}
+
